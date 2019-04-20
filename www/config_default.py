f5835db4f5bb7abb9747938270c219d44d352885
guobird <zhg2008.1@qq.com>
2019-04-20 11:12:06 +0800

    配置文件py 提交by zzgbird 20190420

---
+++ www/config_default.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Michael Liao'  # copy by zzgbird
+
+configs = {
+	'debug': True,
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'www',
+		'password': 'www',
+		'db': 'awesome'		
+	},
+	'session': {
+		'secret': 'Awesome'
+	}
+}
+
+
+
---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Liao'  # copy by zzgbird
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
+
