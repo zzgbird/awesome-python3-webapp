6b26d3c6ea11d546bc359a8ec71c8be489cec041
guobird <zhg2008.1@qq.com>
2019-04-21 18:58:55 +0800

    handlers 添加了/api函数 day09 20190421

--- www/handlers.py
+++ www/handlers.py
@@ -12,8 +12,8 @@ from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
 @get('/')
-def index(request):
-	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+async def index(request):
+	summary =  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 	blogs = [
 		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
 		Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
@@ -24,3 +24,11 @@ def index(request):
 		'__template__': 'blogs.html',
 		'blogs': blogs
 	}
+
+@get('/api/users')
+async def api_get_users():
+	users = await User.findAll(orderBy='created_at desc')
+	for u in users:
+		u.passwd = '******'
+	return dict(users=users)
+
